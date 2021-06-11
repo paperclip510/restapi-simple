e7114af7d01442fd502e1e26e30484df2cc0c49a
kdh <troth1532@naver.com>
2021-06-12 05:48:28 +0900

    dele user using DELETE HTTP Method

--- src/main/java/com/rws/user/UserDaoService.java
+++ src/main/java/com/rws/user/UserDaoService.java
@@ -2,6 +2,7 @@ package com.rws.user;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -9,31 +10,50 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserDaoService {
 	private static List<User> users = new ArrayList<>();
-	
+
 	static {
-		users.add(new User(1,"kdh",new Date()));
-		users.add(new User(2,"kdh2",new Date()));
-		users.add(new User(3,"kdh3",new Date()));
+		users.add(new User(1, "kdh", new Date()));
+		users.add(new User(2, "kdh2", new Date()));
+		users.add(new User(3, "kdh3", new Date()));
 	}
-	
-	public List<User> findAll(){
+
+	// 사용자리스트 조회
+	public List<User> findAll() {
 		return users;
 	}
-	
+
+	// 특정 사용자 조회
 	public User findOne(long id) {
-		for(User user : users) {
-			if(user.getId() == id) {
+		for (User user : users) {
+			if (user.getId() == id) {
 				return user;
 			}
 		}
 		return null;
 	}
-	
+
+	// 사용자 추가
 	public User save(User user) {
-		if(user.getId() == 0) {
-			user.setId(users.size()+1);
+		if (user.getId() == 0) {
+			user.setId(users.size() + 1);
 		}
 		users.add(user);
 		return user;
+	}
+
+	// 사용자 삭제
+	public User deleteById(long id) {
+		Iterator<User> iterator = users.iterator();
+
+		while (iterator.hasNext()) {
+			User user = iterator.next();
+
+			if(user.getId() == id) {
+				iterator.remove();
+				return user;
+			}
+		}
+
+		return null;
 	}
 }
