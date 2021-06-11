43a7a40f6e36b7ed35019fe0fa1ef4b868cb4ee2
kdh <troth1532@naver.com>
2021-06-12 05:36:25 +0900

    AOP Exception handling

---
+++ src/main/java/com/rws/exception/CustomizedResponseEntityExceptioinHandler.java
@@ -0,0 +1,34 @@
+package com.rws.exception;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.rws.user.UserNotFoundException;
+
+@RestController
+@ControllerAdvice // 모든 컨트롤러 실행될때 controllerAdvice가 선언된 클래스가 빈으로 등록된
+public class CustomizedResponseEntityExceptioinHandler extends ResponseEntityExceptionHandler {
+
+	@ExceptionHandler(Exception.class)
+	public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) {
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+
+		return new ResponseEntity<Object>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@ExceptionHandler(UserNotFoundException.class)
+	public final ResponseEntity<Object> handleUserNotFoundException(Exception ex, WebRequest request) {
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+
+		return new ResponseEntity<Object>(exceptionResponse, HttpStatus.NOT_FOUND);
+	}
+		
+
+}
